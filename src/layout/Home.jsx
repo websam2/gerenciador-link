ba3d419a948384998b11a7b0b4b434ab953c86db
Samuel Z <websamrgt@gmail.com>
2024-08-07 15:26:46 -0300

    novos links

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -47,6 +47,7 @@ const Home = () => {
 					<Button href="https://outlook.office.com/mail/">E-mail</Button>
 					<Button href="https://sgi-websam2s-projects.vercel.app/">SGI</Button>
 					<Button href="https://doe.sp.gov.br/sumario">DOE</Button>
+					<Button href="https://www.imprensaoficial.com.br/">Imprensa</Button>
 				</div>
 			</div>
 
